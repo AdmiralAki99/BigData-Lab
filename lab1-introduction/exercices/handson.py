52d98f20b8928dd43ebaa179c7575efddd05ad9c
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2022-01-22 12:52:20 -0500

    Completed Lab 1 with pytests

--- lab1-introduction/exercices/handson.py
+++ lab1-introduction/exercices/handson.py
@@ -4,7 +4,11 @@
 # If numbers equal [10, 10, 15, 7], the return value should be 10.5
 def avg(numbers):
   # +++your code here+++
-  return
+  average = 0
+  for number in numbers:
+    average = average+number
+  average = average/len(numbers)
+  return average
 
 # B. both_ends
 # Given a string s, return a string made of the first 2
@@ -13,7 +17,7 @@ def avg(numbers):
 # is less than 2, return instead the empty string.
 def both_ends(s):
   # +++your code here+++
-  return
+  return (s[0:2]+s[len(s)-2:])
 
 # D. MixUp
 # Given strings a and b, return a single string with a and b separated
@@ -24,7 +28,7 @@ def both_ends(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  return (b + " "+ a)
 
 # B. uniq
 # Given a list of objects (numbers, strings, booleans), return the same list without duplicates.
@@ -33,7 +37,7 @@ def mix_up(a, b):
 # Note that the order does not matter
 def uniq(a):
   # +++your code here+++
-  return
+  return list(set(a))
 
 # B. merge
 # Given two sorted list of numbers, return a sorted list with the elements of both lists.
@@ -41,5 +45,22 @@ def uniq(a):
 # the return value should be [1, 2, 5, 7, 8, 13, 14, 42]
 def merge(a, b):
   # +++your code here+++
-  return
+  tempList = (a+b)
+  tempList.sort()
+  return tempList
 
+def test_avg():
+    assert avg([10, 10, 15, 7]) == 10.5
+
+def test_both_ends():
+    assert both_ends("spring") == "spng"
+
+def test_mix_up():
+    assert mix_up('mix','pod') == "pod mix"
+    assert mix_up('dog', 'dinner') == 'dinner dog'
+
+def test_uniq():
+    assert set(uniq([1, True, "otter", "platypus", 1, 2, False, "otter"])) == set([False, 1, 2, 'otter', 'platypus'])
+
+def test_merge():
+    assert merge([1, 5, 13, 14],[2, 7, 8, 42]) == [1, 2, 5, 7, 8, 13, 14, 42]
